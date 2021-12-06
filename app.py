c8a5365be6a139899971fecd751726c133667ec1
20186322 <20186322@itla.edu.do>
2021-12-05 21:41:34 -0400

    first commit

--- app.py
+++ app.py
@@ -10,16 +10,13 @@ app = FastAPI()
 posts = []
 
 class Post(BaseModel):
+    Nombre: str
     Fecha: str
-    Cliente_id: str
-    Descripcion: str
-    Subtotal: str
-    Itbis: str
-    Total: str
+    Comentario: str
 
 @app.get('/')
 def read_root():
-    return {"Bienvenidos": "Bienvenidos a mi API"}
+    return {"Bienvenidos": "Bienvenidos a mi proyecto final API"}
 
 @app.get('/posts')
 def get_posts():
@@ -36,25 +33,23 @@ def get_post(post_id: str):
     for post in posts:
         if post["id"] == post_id:
             return post
-    raise HTTPException(status_code=404, detail="Item not found")
+    raise HTTPException(status_code=404, detail="La pelicula no existe")
 
 @app.delete('/posts/{post_id}')
 def delete_post(post_id: str):
     for index, post in enumerate(posts):
         if post["id"] == post_id:
             posts.pop(index)
-            return {"message": "Factura ha sido eliminada exitosamente"}
-    raise HTTPException(status_code=404, detail="Item not found")
+            return {"message": "La pelicula ha sido eliminada exitosamente"}
+    raise HTTPException(status_code=404, detail="La pelicula no existe")
 
 @app.put('/posts/{post_id}')
 def update_post(post_id: str, updatedPost: Post):
     for index, post in enumerate(posts):
         if post["id"] == post_id:
+            posts[index]["Nombre"]= updatedPost.dict()["Nombre"]
             posts[index]["Fecha"]= updatedPost.dict()["Fecha"]
-            posts[index]["Cliente_id"]= updatedPost.dict()["Cliente_id"]
-            posts[index]["Descripcion"]= updatedPost.dict()["Descripcion"]
-            posts[index]["Subtotal"]= updatedPost.dict()["Subtotal"]
-            posts[index]["Itbis"]= updatedPost.dict()["Itbis"]
-            posts[index]["Total"]= updatedPost.dict()["Total"]
-            return {"message": "La factura se ha actualizado exitosamente"}
-    raise HTTPException(status_code=404, detail="Item not found")
+            posts[index]["Comentario"]= updatedPost.dict()["Comentario"]
+           
+            return {"message": "La pelicula se ha actualizado exitosamente"}
+    raise HTTPException(status_code=404, detail="La pelicula no existe")
